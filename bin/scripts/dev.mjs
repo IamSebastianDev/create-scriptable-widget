78453fc2d3faf812d5010922cd0421447fb6e85f
Sebastian <sebastian@iamsebastian.dev>
2022-02-07 22:20:16 +0100

    PROG: Refractored development script to better incorparate the bundler

--- bin/scripts/dev.mjs
+++ bin/scripts/dev.mjs
@@ -8,39 +8,45 @@
  *  - Invoking RollUp
  */
 
+import { watch } from 'node:fs/promises';
 import { spawnAsync } from '../internals/utilities/spawnAsync.mjs';
 import { presentDetails, colours } from 'dev-server-details';
-import { sleep } from '../internals/utilities/sleep.mjs';
 import config from '../../scriptable.config.js';
+import { developmentMessage } from './cliOutputs.mjs';
 
-// Starts the bundler in watch mode for incremental builds
+/**
+ * Build the inital bundle for deployment, and create the fileWatcher that will incremently rebuild the bundle
+ * when a change in the src folder is detected.
+ */
 
-const onRollup = ({ type, data }) => {};
-spawnAsync('rollup --config rollup.config.js --watch', undefined, onRollup);
+const rootPath = config.input
+	.split('/')
+	.filter((elem) => !elem.includes('.'))
+	.join('/');
+
+await spawnAsync('rollup --config rollup.config.js');
+(async () => {
+	const { signal } = new AbortController();
+	const watcher = watch(rootPath, { signal, recursive: true });
+	for await (const event of watcher) {
+		try {
+			spawnAsync('rollup --config rollup.config.js');
+		} catch (err) {
+			if (err.name === 'AbortError') return;
+			throw err;
+		}
+	}
+})();
 
 // Starts the server
-
-const onServer = ({ type, data }) => {};
-spawnAsync('node ./bin/server/server.mjs', undefined, onServer);
+spawnAsync('node ./bin/server/server.mjs');
 
 // Build the dev slug
+spawnAsync('node ./bin/internals/createSlug.mjs');
 
-const onBuild = ({ type, data }) => {};
-spawnAsync('node ./bin/internals/createSlug.mjs', undefined, onBuild);
-
-await sleep(10000);
 presentDetails({
 	PORT: process.env.PORT || config.server.port,
-	userText: {
-		heading: '{{time}} create-scriptable-app: 🚀',
-		main: !config.server.headless
-			? 'Dashboard is now available at:'
-			: 'App runs in headless mode. No dashboard is served.',
-		text: config.server.headless
-			? []
-			: ['Local:             {{local}}', 'On your network:   {{ip}}'],
-		notice: 'Copy the contents of ./scriptable/dev.slug.js into a fresh Scriptable widget to complete the setup process.',
-	},
+	userText: developmentMessage,
 	userTheme: {
 		notice: colours.text.green,
 	},
