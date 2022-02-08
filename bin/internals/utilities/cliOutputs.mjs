eaef531b20fa77e243e2dbe29b64e8ac7b943d8e
Sebastian <sebastian@iamsebastian.dev>
2022-02-08 15:19:20 +0100

    FEAT: Refractored the CLIOutput text for the server

---
+++ bin/internals/utilities/cliOutputs.mjs
@@ -0,0 +1,17 @@
+/** @format */
+
+import path from 'node:path';
+import config from '../../../scriptable.config.js';
+
+const outputPath = path.join(config.dev, config.widget.name + '.dev.widget.js');
+
+export const developmentMessage = {
+	heading: '[{{time}}] create-scriptable-app: 🚀',
+	main: !config.server.headless
+		? 'Dashboard is now available at:'
+		: 'App runs in headless mode. No dashboard is served.',
+	text: config.server.headless
+		? []
+		: ['Local:             {{local}}', 'On your network:   {{ip}}'],
+	notice: `Copy the contents of ${outputPath} into a fresh Scriptable widget to complete the setup process.`,
+};
